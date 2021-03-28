1d325d09c2b91db6e22a4b73b06d3da3fd688b6d
akubicz <akubicz@users.noreply.github.com>
2021-03-28 11:08:10 +0000

    poprawki

--- jenkins_get_version.py
+++
@@ -1,9 +0,0 @@
-import jenkins
-
-server = jenkins.Jenkins('http://127.0.0.1:8080/',
-                         username='admin',
-                         password='pass')
-
-user = server.get_whoami()
-version = server.get_version()
-print('Hello %s from Jenkins %s' % (user['fullName'], version))
